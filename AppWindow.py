a7faf021982c7ed7355c6ee185a98600017a2f1b
kisielew <you@example.com>
2017-11-09 19:45:37 +0100

    Added hh:mm timer

--- AppWindow.py
+++ AppWindow.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import time
+import datetime
 from Tkinter import *
 from Customer import Customer
 from DataBaseController import DataBaseController
@@ -15,14 +16,14 @@ def updateList():
      # for entry in lbIceRingMembers:
         # listbox.delete(entry)
         # print entry
-    print type(lbIceRingMembers)
     pass
 
 def getLocalTime():
-    return time.asctime(time.localtime(time.time()))
+    # return time.asctime(time.localtime(time.time()))
+    return time.localtime(time.time())
 
 def getTimeAfterHour():
-    return time.asctime(time.localtime(time.time() + 3600))
+    return time.localtime(time.time() + 3600)
 
 def set_list(event):
     listbox.delete(0, END)
@@ -33,6 +34,28 @@ def set_list(event):
         listbox.insert(0, entity)
         pass
 
+def update2ndList():
+    for i, entity in enumerate(lbIceRingMembers.get(0, END)):
+        newEntity = list(entity)
+        # newEntity.pop(1)
+        # newEntity.insert(1, time.asctime(getLocalTime()))
+
+        # seltext += [time.mktime(end_dt) - time.mktime(start_dt)]
+        dateString = newEntity[2]
+        parsedTime = time.strptime(dateString, '%a %b %d %H:%M:%S %Y')
+        time.mktime(parsedTime)
+
+        timeDiff = time.mktime(parsedTime) - time.mktime(getLocalTime())
+        m, s = divmod(timeDiff, 60)
+        timeToInsert = '%02d:%02d' % (m, s)
+        newEntity.pop(3)
+        newEntity.insert(3, timeToInsert)
+        # print newEntity
+
+        lbIceRingMembers.delete(i)
+        lbIceRingMembers.insert(i, tuple(newEntity))
+    root.after(1000, update2ndList)
+
 def get_list(event):
     """
     function to read the listbox selection
@@ -42,11 +65,16 @@ def get_list(event):
     index = listbox.curselection()[0]
     # get the line's text
     seltext = [listbox.get(index)]
-    seltext += [getLocalTime(), getTimeAfterHour()]
+    seltext += [time.asctime(getLocalTime()), time.asctime(getTimeAfterHour())]
+    start_dt = getLocalTime()
+    end_dt = getTimeAfterHour()
+    print time.mktime(end_dt) - time.mktime(start_dt)
+    seltext += [time.mktime(end_dt) - time.mktime(start_dt)]
     # delete previous text in enter1
     enter1.delete(0, 50)
     # now display the selected text
     lbIceRingMembers.insert(0, tuple(seltext))
+    update2ndList()
 
 def onDouble(event):
     widget = event.widget
